a6edc99e2989bf997e2163742b7c044aa0ecee89
Guilherme de Souza <guigasouza222@gmail.com>
2020-04-07 16:03:40 -0600

    cat left container

--- src/left-container.js
+++ src/left-container.js
@@ -11,9 +11,13 @@ function Left(props) {
     const categories = {}
     const Camp_AR = Object.values(context.campaigns)
     const Cat_AR = Object.values(context.categories)
+    let c = 0
+    let cat = ""
 
-    for (let p of Camp_AR) {
-        categories[p.category_id] = (categories[p.category_id] || 0) + 1
+    for (let p of Camp_AR) {        
+        cat = !Cat_AR[c] ? "Other" : Cat_AR[c].title
+        categories[cat] = (categories[cat] || 0) + 1
+        c+=1
     }
     
     return(
@@ -22,19 +26,11 @@ function Left(props) {
         <Link to="/" className="nav-link"><h5>All Campaigns ({Camp_AR.length})</h5></Link>
         <bs.Nav defaultActiveKey="/home" link="black" className="flex-column text-dark">
                 {Object.entries(categories).map( ([key, value]) => (      
-                    <Link style={{color: "black"}} key={key} to={`/category/${!Cat_AR[parseInt(key)] ? 0 : Cat_AR[parseInt(key)].id}`} className="nav-link">{!Cat_AR[parseInt(key)] ? "Other" : Cat_AR[parseInt(key)].title} ({value})</Link>
-                    //console.log(!Cat_AR[parseInt(key)] ? "Other" : Cat_AR[parseInt(key)].title )
-                    //console.log(value)
+                    <Link style={{color: "black"}} key={key} to={`/category/${key}`} className="nav-link">{key} ({value})</Link>
                     )                                            
                 
                 )}
         </bs.Nav>
-        
-        {/* {Object.entries(context.categories).map((category) => {
-            return (
-            <Link to={"/category/" + category[1].title} className="nav-link" key={category[1].title}>{category[1].title + ' (' + category[1].count +')'}</Link>
-            )
-        })}  */}
         </bs.Nav>
         
     )
